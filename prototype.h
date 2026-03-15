cc3c062464de7989dc8f03965867cf1c9e511439
IDRISSI MELIANI Hamza <idrissimeliani.hamza.eleve@ehtp.ac.ma>
2026-03-15 15:14:08 +0000

    V7

--- prototype.h
+++ prototype.h
@@ -1,6 +1,5 @@
 #ifndef PROTOTYPE
 #define PROTOTYPE
-
 // Couleurs des phrases
 #define BLK "\e[0;30m"
 #define RED "\e[0;31m"
@@ -11,7 +10,6 @@
 #define CYN "\e[0;36m"
 #define WHT "\e[0;37m"
 #define COLOR_RESET "\e[0m"
-
 // Couleurs de l'arriere plan
 #define BG_RED   "\e[41m"
 #define BG_GRN   "\e[42m"
@@ -20,18 +18,15 @@
 #define BG_MAG   "\e[45m"
 #define BG_CYN   "\e[46m"
 #define BG_WHT   "\e[47m"
-
 #define Nbr_Cases 24
 #define Nbr_Moulin 16
 #define Max_Adjascent 4
-
 typedef enum
 {
     vide,
     J1,
     J2
-} etatCase;
-
+}etatCase;
 typedef struct
 {
     char Pl[11][30];
@@ -39,15 +34,13 @@ typedef struct
     int Adjs[Nbr_Cases][Max_Adjascent];
     int Nbr_Adjs[Nbr_Cases];
     int Moulin[Nbr_Moulin][3];
-} Plateau;
-
+}Plateau;
 typedef enum
 {
     humain,
     IA_simple,
     IA_defficile
-} typeJoueur;
-
+}typeJoueur;
 typedef struct
 {
     char Nom[30];
@@ -55,15 +48,13 @@ typedef struct
     int nbrPionPlateau;
     int nbrPionPlacer;
     typeJoueur type;
-} Joueur;
-
+}Joueur;
 typedef enum
 {
     Placement,
     Deplacement,
     Vol
-} PhaseJeu;
-
+}PhaseJeu;
 typedef struct
 {
     Plateau P;
@@ -72,35 +63,28 @@ typedef struct
     int JoueurCourant;// 0: joueur1 | 1: joueur2
     int FinJeu;// 0: pas encore terminee | 1: terminee
     int Gagnant;// -1: pas de gagnant encore | 0: joueur 1 gagne|1:joueur 2 gagne
-} Jeu;
-
-void Nom_jeu(void);
-void menu(void);
-void regles(void);
-int choix_mode(void);
-void Jouer(void);
-
+}Jeu;
+void Nom_jeu();
+void menu();
+void regles();
+int choix_mode();
+void Jouer();
 //Plateau
-void initPlateau(Plateau *);
-int PlateauEstVide(const Plateau *, int);
-void PlacerPionPlateau(Plateau *, int, etatCase);
-void SupprimerPionPlateau(Plateau *, int);
+void initPlateau(Plateau * );
+int PlateauEstVide(const Plateau * , int );
+void PlacerPionPlateau(Plateau * , int  , etatCase );
+void SupprimerPionPlateau(Plateau* , int);
 void afficherPlateau(const Plateau *);
-
 //Joueur
-void initJoueur(Joueur *, int, typeJoueur);
-int nbrTotalePions(const Joueur *);
-
+void initJoueur(Joueur* , int , typeJoueur);
+int nbrTotalePions(const Joueur* );
 //Jeu
-void initJeu(Jeu *, typeJoueur t1, typeJoueur t2);
-void changerJoueurJeu(Jeu *);
-
+void initJeu(Jeu* , typeJoueur t1 , typeJoueur t2);
+void changerJoueurJeu(Jeu*);
 // verification
-int verifieMoulin(const Plateau *, int, etatCase);
-
-// Placement / Deplacement / Vol
-int mouvementValide(const Plateau *, int, int, etatCase);
-int Volvalide(const Plateau *, int, int, etatCase);
+int verifieMoulin(const Plateau * ,int, etatCase);
+// Placement:
+int mouvementValide(const Plateau *, int , int, etatCase);
+int Volvalide(const Plateau *, int , int , etatCase);
 int estCapturable(const Plateau *, int, etatCase);
-
 #endif
